bc4274c78883c3eb12a17e510ead8384f4f1a8ff
ge3224 <ge3224@gmail.com>
2024-03-25 14:25:45 -0500

    Implement functionality for <table> constructor

---
+++ src/_ui/core/table.test.ts
@@ -0,0 +1,50 @@
+// @vitest-environment happy-dom
+
+import { expect, test } from "vitest";
+import Table from "./table";
+import { HTMLElementAttributes } from "@/_definitions/attributes";
+
+test("basic construction", () => {
+  const mock = Table(document.createElement("tr"), {});
+
+  expect(mock).not.toBeNull();
+  expect(mock.tagName).toEqual("TABLE");
+});
+
+test("construct with children", () => {
+
+  const allowedChildrenTypes = [
+    document.createElement("caption"),
+    document.createElement("thead"),
+    document.createElement("tbody"),
+    document.createElement("tfoot"),
+    document.createElement("tr"),
+  ];
+
+  let mockParent: HTMLTableElement;
+
+  allowedChildrenTypes.forEach(mockChild => {
+    mockParent = Table(mockChild, {});
+    expect(mockParent.firstElementChild).not.toBeNull();
+  });
+
+  mockParent = Table(allowedChildrenTypes, {});
+
+  expect(mockParent.firstElementChild).not.toBeNull();
+
+  const unallowed = document.createElement("td");
+
+  mockParent = Table(unallowed, {});
+
+  expect(mockParent.childNodes.length).toBe(0);
+});
+
+test("construction with attributes", () => {
+  const mock = Table(document.createElement("tr"), {
+    id: "bar",
+    class: "foo bar baz",
+  } as HTMLElementAttributes);
+
+  expect(mock.getAttribute("id")).toBe("bar");
+  expect(mock.getAttribute("class")).toBe("foo bar baz");
+});
---
+++ src/_ui/core/table.ts
@@ -0,0 +1,54 @@
+import { HTMLElementAttributes } from "@/_definitions/attributes";
+
+/**
+ * A constructor for the <table> element.
+ *
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/table)
+ */
+export default function Table(
+  children:
+    | HTMLTableCaptionElement
+    | HTMLTableSectionElement
+    | HTMLTableRowElement
+    | Array<
+        HTMLTableCaptionElement | HTMLTableSectionElement | HTMLTableRowElement
+      >,
+  attributes: HTMLElementAttributes
+): HTMLTableElement {
+  const table = document.createElement("table");
+
+  const allowedChildrenTypes = ["CAPTION", "THEAD", "TBODY", "TFOOT", "TR"];
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (allowedChildrenTypes.includes(child.nodeName)) {
+        table.appendChild(child);
+      } else {
+        childWarning(child);
+      }
+    });
+  } else if (children instanceof Node) {
+    const child = children;
+    if (allowedChildrenTypes.includes(child.nodeName)) {
+      table.appendChild(child);
+    } else {
+      childWarning(child);
+    }
+  }
+
+  Object.entries(attributes).map(([key, value]) => {
+    switch (key) {
+      default:
+        table.setAttribute(
+          key.toLowerCase(),
+          typeof value === "number" ? value.toString() : value
+        );
+    }
+  });
+
+  return table;
+}
+
+function childWarning(child: Node) {
+  console.warn(`Warning: unallowed node type: '${child.nodeName}'`);
+}
