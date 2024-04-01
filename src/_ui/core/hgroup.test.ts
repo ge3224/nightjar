9c1130851d7262dd9cf196581ac73edbd0a6318b
ge3224 <ge3224@gmail.com>
2024-03-31 22:11:01 -0500

    Implement functionality for <hgroup> constructor

---
+++ src/_ui/core/hgroup.test.ts
@@ -0,0 +1,87 @@
+/**
+ * Project: Nightjar
+ * Author: Jacob Benison
+ * Copyright: (C) 2024 Jacob Benison
+ * License: MIT
+ *
+ * Unit tests for the _ui/core/hgroup module
+ */
+
+import { expect, test } from "vitest";
+import Hgroup from "./hgroup";
+
+// @vitest-environment happy-dom
+
+test("basic construction", () => {
+  const mock = Hgroup()();
+
+  expect(mock).not.toBeNull();
+
+  expect(mock.tagName).toEqual("HGROUP");
+});
+
+test("construction with attributes", () => {
+  const mock = Hgroup(null, {
+    id: "bar",
+    class: "foo bar baz",
+  })();
+
+  expect(mock.getAttribute("id")).toBe("bar");
+  expect(mock.getAttribute("class")).toBe("foo bar baz");
+});
+
+test("construction with a heading", () => {
+  const mock = Hgroup(document.createElement("h1"))();
+
+  expect(mock.childNodes.length).toBe(1);
+});
+
+test("construction with a paragraph followed by a heading", () => {
+  const content = (() => {
+    const p = document.createElement("p");
+    const h2 = document.createElement("h2");
+    return [p, h2];
+  })();
+
+  const mock = Hgroup(content)();
+
+  expect(mock.childNodes.length).toBe(2);
+});
+
+test("construction with a paragraph followed by a heading, followed by a paragraph", () => {
+  const content = (() => {
+    const p = document.createElement("p");
+    const h2 = document.createElement("h2");
+    const p2 = document.createElement("p");
+    return [p, h2, p2];
+  })();
+
+  const mock = Hgroup(content)();
+
+  expect(mock.childNodes.length).toBe(3);
+});
+
+test("construction with a paragraphs and no heading", () => {
+  const content = (() => {
+    const p = document.createElement("p");
+    const p2 = document.createElement("p");
+    return [p, p2];
+  })();
+
+  const mock = Hgroup(content)();
+
+  expect(mock.childNodes.length).toBe(0);
+});
+
+test("construction with paragraphs and two headings", () => {
+  const content = (() => {
+    const p = document.createElement("p");
+    const h2 = document.createElement("h2");
+    const _h2 = document.createElement("h2");
+    return [h2, p, _h2];
+  })();
+
+  const mock = Hgroup(content)();
+
+  expect(mock.childNodes.length).toBe(2);
+});
---
+++ src/_ui/core/hgroup.ts
@@ -0,0 +1,83 @@
+/**
+ * Project: Nightjar
+ * Author: Jacob Benison
+ * Copyright: (C) 2024 Jacob Benison
+ * License: MIT
+ */
+
+import { HTMLElementAttributes } from "@/_definitions/attributes";
+import { NewHTMLElement } from "@/_definitions/constructors";
+
+/**
+ * Returns a constructor for the HTML <hgroup> element.
+ *
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/hgroup)
+ */
+
+export default function Hgroup(
+  children: Node | Array<Node> | null = null,
+  attributes: HTMLElementAttributes = {}
+): NewHTMLElement {
+  return (): HTMLElement => {
+    const hgroup = document.createElement("hgroup");
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      switch (key) {
+        case "autofocus":
+          hgroup.autofocus = value;
+          return;
+        case "inert":
+          hgroup.inert = value;
+          return;
+        default:
+          hgroup.setAttribute(
+            key.toLowerCase(),
+            typeof value === "number" ? value.toString() : value
+          );
+      }
+    });
+
+    if (!children) return hgroup;
+
+    // TODO: Zero or more <p> elements, followed by one h1, h2, h3, h4, h5, or h6 element, followed by zero or more <p> elements.
+    const permittedContent = ["P", "H1", "H2", "H3", "H4", "H5", "H6"];
+
+    if (!Array.isArray(children)) {
+      if (
+        children instanceof Node &&
+        permittedContent.indexOf(children.nodeName) > 0
+      ) {
+        hgroup.append(children);
+      }
+      return hgroup;
+    }
+
+    const { permitted, hasHeading } = children.reduce(
+      (
+        collect: { permitted: Array<Node>; hasHeading: boolean },
+        child: Node
+      ) => {
+        const { permitted, hasHeading } = collect;
+
+        if (permittedContent.indexOf(child.nodeName) > 0 && !hasHeading) {
+          permitted.push(child);
+          return { permitted, hasHeading: true };
+        }
+
+        if (permittedContent.indexOf(child.nodeName) === 0) {
+          permitted.push(child);
+          return { ...collect, permitted };
+        }
+
+        return collect;
+      },
+      { permitted: [], hasHeading: false }
+    );
+
+    if (hasHeading) {
+      permitted.forEach((child) => hgroup.append(child));
+    }
+
+    return hgroup;
+  };
+}
