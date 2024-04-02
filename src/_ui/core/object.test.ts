df58b107e8f9c10cb470a737883693c5365d64d2
ge3224 <ge3224@gmail.com>
2024-04-02 17:46:11 -0500

    Refactor unit tests for _ui/core/object

--- src/_ui/core/object.test.ts
+++ src/_ui/core/object.test.ts
@@ -4,7 +4,7 @@
  * Copyright: (C) 2024 Jacob Benison
  * License: MIT
  *
- * Unit tests for the _ui/core/menu module
+ * Unit tests for the _ui/core/object module
  */
 
 import { test, assert } from "vitest"; // Destructure test and assert from vitest
@@ -13,18 +13,18 @@ import NewObject from "./object";
 
 // @vitest-environment happy-dom
 
-test("Basic construction test for NewMenu", () => {
+test("Basic construction test for NewObject", () => {
   // Test case: Testing basic construction with no children and no attributes
-  const menu = NewObject()();
-  assert.equal(menu.tagName, OBJECT);
-  assert.equal(menu.childNodes.length, 0);
+  const obj = NewObject()();
+  assert.equal(obj.tagName, OBJECT);
+  assert.equal(obj.childNodes.length, 0);
 });
 
-test("Construction with attributes test for NewMenu", () => {
+test("Construction with attributes test for NewObject", () => {
   // Test case: Testing construction with attributes only
   const attributes = {
-    id: "menu1",
-    class: "menu-class",
+    id: "obj1",
+    class: "obj-class",
     autofocus: true,
     data: "foo",
     form: "bar",
@@ -33,27 +33,27 @@ test("Construction with attributes test for NewMenu", () => {
     type: "foobarbaz",
     width: "100px",
   };
-  const menu = NewObject(undefined, attributes)();
-  assert.equal(menu.tagName, OBJECT);
-  assert.equal(menu.getAttribute("id"), "menu1");
-  assert.equal(menu.getAttribute("class"), "menu-class");
-  assert.equal(menu.autofocus, true);
-  assert.equal(menu.getAttribute("data"), "foo");
-  assert.equal(menu.getAttribute("form"), "bar");
-  assert.equal(menu.getAttribute("height"), "100px");
-  assert.equal(menu.getAttribute("name"), "baz");
-  assert.equal(menu.getAttribute("type"), "foobarbaz");
-  assert.equal(menu.getAttribute("width"), "100px");
-  assert.equal(menu.childNodes.length, 0);
+  const obj = NewObject(undefined, attributes)();
+  assert.equal(obj.tagName, OBJECT);
+  assert.equal(obj.getAttribute("id"), "obj1");
+  assert.equal(obj.getAttribute("class"), "obj-class");
+  assert.equal(obj.autofocus, true);
+  assert.equal(obj.getAttribute("data"), "foo");
+  assert.equal(obj.getAttribute("form"), "bar");
+  assert.equal(obj.getAttribute("height"), "100px");
+  assert.equal(obj.getAttribute("name"), "baz");
+  assert.equal(obj.getAttribute("type"), "foobarbaz");
+  assert.equal(obj.getAttribute("width"), "100px");
+  assert.equal(obj.childNodes.length, 0);
 });
 
-test("Construction with children test for NewMenu", () => {
+test("Construction with children test for NewObject", () => {
   // Test case: Testing construction with children only
   const child1 = document.createElement(DEL);
   const child2 = document.createElement(PARAM);
-  const menu = NewObject([child1, child2])();
-  assert.equal(menu.tagName, OBJECT);
-  assert.equal(menu.childNodes.length, 2);
-  assert.equal(menu.childNodes[0], child2);
-  assert.equal(menu.childNodes[1], child1);
+  const obj = NewObject([child1, child2])();
+  assert.equal(obj.tagName, OBJECT);
+  assert.equal(obj.childNodes.length, 2);
+  assert.equal(obj.childNodes[0], child2);
+  assert.equal(obj.childNodes[1], child1);
 });
