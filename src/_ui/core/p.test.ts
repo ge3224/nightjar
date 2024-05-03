d54ea22e557b599aa62ff590e76d809df24d0e20
ge3224 <ge3224@gmail.com>
2024-05-03 12:08:31 -0500

    Expermiment with patterns

--- src/_ui/core/p.test.ts
+++ src/_ui/core/p.test.ts
@@ -5,98 +5,101 @@ import { EM, P, SPAN } from "@/_lib/node_names";
 // @vitest-environment happy-dom
 
 test("basic construction", () => {
-  const paragraph = p().build();
+  const paragraph = p().create();
 
-  assert.equal(paragraph.tagName, P);
+  assert.equal(paragraph, "");
+
 });
 
-test("construction with attributes", () => {
-  const paragraph = p().attributes({ id: "foo" }).build();
-
-  assert.equal(paragraph.id, "foo");
-});
-
-test("construction with basic contents", () => {
-  const paragraph = p().children("foo").build();
-
-  assert.equal(paragraph.textContent, "foo");
-});
-
-test("construction with multiple child nodes", () => {
-  const foo = "foo";
-
-  const bar = document.createElement("span");
-  bar.textContent = "bar";
-
-  const baz = document.createElement("em");
-  baz.textContent = "baz";
-
-  const paragraph = p().children(foo, bar, baz).build();
-
-  assert.equal(paragraph.childNodes.length, 3);
-
-  assert.equal(paragraph.childNodes[0].textContent, "foo");
-
-  assert.equal(paragraph.childNodes[1].nodeName, SPAN);
-  assert.equal(paragraph.childNodes[1].textContent, "bar");
-
-  assert.equal(paragraph.childNodes[2].nodeName, EM);
-  assert.equal(paragraph.childNodes[2].textContent, "baz");
-});
-
-test("construction with boolean attributes", () => {
-  const paragraph = p().attributes({ autofocus: true, inert: false }).build();
-
-  assert.isTrue(paragraph.autofocus);
-  assert.isFalse(paragraph.inert);
-});
-
-test("construction with numeric attributes", () => {
-  const paragraph = p().attributes({ tabindex: 3 }).build();
-
-  assert.equal(paragraph.getAttribute("tabindex"), "3");
-});
-
-// TODO: define event attributes
+// test("construction with attributes", () => {
+//   const paragraph = p().attributes({ id: "foo" }).build();
 //
-// test("construction with event attributes", () => {
-//   const onClick = () => {
-//     console.log("Paragraph clicked");
-//   };
-//
-//   const paragraph = p().attributes({ onclick: onClick }).build();
-//
-//   assert.isFunction(paragraph.onclick);
+//   assert.equal(paragraph.id, "foo");
 // });
-
-test("construction with nested elements", () => {
-  const nestedContent = [
-    "Nested text content",
-    document.createElement(SPAN),
-    document.createElement(EM)
-  ];
-
-  const result = p().children(...nestedContent).build();
-
-  assert.equal(result.tagName, P);
-  assert.equal(result.childNodes.length, 3);
-
-  // Check the nested phrasing content
-  assert.equal(result.childNodes[0].textContent, "Nested text content");
-  assert.equal(result.childNodes[1].nodeName, SPAN);
-  assert.equal(result.childNodes[2].nodeName, EM);
-});
-
-// TODO: account for null and undefined arguments being passed to p functions
 //
-// test("construction with null or undefined attributes and children", () => {
-//   const paragraphWithNullAttributes = p(null).build();
-//   const paragraphWithUndefinedAttributes = p(undefined).build();
-//   const paragraphWithNullChildren = p().children(null).build();
-//   const paragraphWithUndefinedChildren = p().children(undefined).build();
+// test("construction with basic contents", () => {
+//   const paragraph = p().children("foo").build();
 //
-//   assert.equal(paragraphWithNullAttributes.childNodes.length, 0);
-//   assert.equal(paragraphWithUndefinedAttributes.childNodes.length, 0);
-//   assert.equal(paragraphWithNullChildren.childNodes.length, 0);
-//   assert.equal(paragraphWithUndefinedChildren.childNodes.length, 0);
+//   assert.equal(paragraph.textContent, "foo");
 // });
+//
+// test("construction with multiple child nodes", () => {
+//   const foo = "foo";
+//
+//   const bar = document.createElement("span");
+//   bar.textContent = "bar";
+//
+//   const baz = document.createElement("em");
+//   baz.textContent = "baz";
+//
+//   const paragraph = p().children(foo, bar, baz).build();
+//
+//   assert.equal(paragraph.childNodes.length, 3);
+//
+//   assert.equal(paragraph.childNodes[0].textContent, "foo");
+//
+//   assert.equal(paragraph.childNodes[1].nodeName, SPAN);
+//   assert.equal(paragraph.childNodes[1].textContent, "bar");
+//
+//   assert.equal(paragraph.childNodes[2].nodeName, EM);
+//   assert.equal(paragraph.childNodes[2].textContent, "baz");
+// });
+//
+// test("construction with boolean attributes", () => {
+//   const paragraph = p().attributes({ autofocus: true, inert: false }).build();
+//
+//   assert.isTrue(paragraph.autofocus);
+//   assert.isFalse(paragraph.inert);
+// });
+//
+// test("construction with numeric attributes", () => {
+//   const paragraph = p().attributes({ tabindex: 3 }).build();
+//
+//   assert.equal(paragraph.getAttribute("tabindex"), "3");
+// });
+//
+// // TODO: define event attributes
+// //
+// // test("construction with event attributes", () => {
+// //   const onClick = () => {
+// //     console.log("Paragraph clicked");
+// //   };
+// //
+// //   const paragraph = p().attributes({ onclick: onClick }).build();
+// //
+// //   assert.isFunction(paragraph.onclick);
+// // });
+//
+// test("construction with nested elements", () => {
+//   const nestedContent = [
+//     "Nested text content",
+//     document.createElement(SPAN),
+//     document.createElement(EM),
+//   ];
+//
+//   const result = p()
+//     .children(...nestedContent)
+//     .build();
+//
+//   assert.equal(result.tagName, P);
+//   assert.equal(result.childNodes.length, 3);
+//
+//   // Check the nested phrasing content
+//   assert.equal(result.childNodes[0].textContent, "Nested text content");
+//   assert.equal(result.childNodes[1].nodeName, SPAN);
+//   assert.equal(result.childNodes[2].nodeName, EM);
+// });
+//
+// // TODO: account for null and undefined arguments being passed to p functions
+// //
+// // test("construction with null or undefined attributes and children", () => {
+// //   const paragraphWithNullAttributes = p(null).build();
+// //   const paragraphWithUndefinedAttributes = p(undefined).build();
+// //   const paragraphWithNullChildren = p().children(null).build();
+// //   const paragraphWithUndefinedChildren = p().children(undefined).build();
+// //
+// //   assert.equal(paragraphWithNullAttributes.childNodes.length, 0);
+// //   assert.equal(paragraphWithUndefinedAttributes.childNodes.length, 0);
+// //   assert.equal(paragraphWithNullChildren.childNodes.length, 0);
+// //   assert.equal(paragraphWithUndefinedChildren.childNodes.length, 0);
+// // });
--- src/_ui/core/p.ts
+++ src/_ui/core/p.ts
@@ -14,7 +14,6 @@ import { isPhrasingContent } from "@/_lib/content";
 import { setElementAttributes } from "@/_lib/html_elements";
 import { P } from "@/_lib/node_names";
 
-
 /**
  * Helper function to append children to an HTML element.
  * @param parent The parent HTML element to which children will be appended.
@@ -37,49 +36,71 @@ const appendChildren = (
   return parent as HTMLParagraphElement;
 };
 
-/**
- * Function to create a new paragraph element builder.
- * @param attributes An optional object containing attributes to set on the paragraph element.
- * @param children Optional children (strings or DOM nodes) to append to the paragraph element.
- * @returns A builder object with methods to set attributes, append children, and build the final paragraph element.
- */
-const p = (
-  attributes: HTMLElementGlobalAttributes = {},
-  ...children: Array<string | Node>
-): ParagraphBuilder => {
-  const paragraph = document.createElement(P) as HTMLParagraphElement;
-  attributes as HTMLElementAttributes;
-  appendChildren(paragraph, children);
+function p() {
+  return {
+    attributes: (input: HTMLElementGlobalAttributes) => p,
+    children: (...input: Array<String | Node>) => p,
+    create: (
+      attributes?: (paragraph: HTMLParagraphElement) => void,
+      children?: (paragraph: HTMLParagraphElement) => void
+    ) => {
+      const p = document.createElement(P) as HTMLParagraphElement;
 
-  const builder: ParagraphBuilder = {
-    /**
-     * Method to set attributes on the paragraph element.
-     * @param newAttributes An object containing attributes to set on the paragraph element.
-     * @returns The builder object for method chaining.
-     */
-    attributes(newAttributes: HTMLElementGlobalAttributes) {
-      setElementAttributes(paragraph, newAttributes as HTMLElementAttributes);
-      return builder;
-    },
-    /**
-     * Method to append children to the paragraph element.
-     * @param newChildren Additional children (strings or DOM nodes) to append to the paragraph element.
-     * @returns The builder object for method chaining.
-     */
-    children(...newChildren: Array<string | Node>) {
-      appendChildren(paragraph, newChildren);
-      return builder;
-    },
-    /**
-     * Method to build and return the final HTML paragraph element.
-     * @returns The final HTML paragraph element with all specified attributes and children.
-     */
-    build() {
-      return paragraph;
+      if (attributes) {
+        attributes(p);
+      }
+
+      if (children) {
+        children(p);
+      }
+      return p;
     },
   };
+}
 
-  return builder;
-};
+// /**
+//  * Function to create a new paragraph element builder.
+//  * @param attributes An optional object containing attributes to set on the paragraph element.
+//  * @param children Optional children (strings or DOM nodes) to append to the paragraph element.
+//  * @returns A builder object with methods to set attributes, append children, and build the final paragraph element.
+//  */
+// const p = (
+//   attributes: HTMLElementGlobalAttributes = {},
+//   ...children: Array<string | Node>
+// ): ParagraphBuilder => {
+//   const paragraph = document.createElement(P) as HTMLParagraphElement;
+//   attributes as HTMLElementAttributes;
+//   appendChildren(paragraph, children);
+//
+//   const builder: ParagraphBuilder = {
+//     /**
+//      * Method to set attributes on the paragraph element.
+//      * @param newAttributes An object containing attributes to set on the paragraph element.
+//      * @returns The builder object for method chaining.
+//      */
+//     attributes(newAttributes: HTMLElementGlobalAttributes) {
+//       setElementAttributes(paragraph, newAttributes as HTMLElementAttributes);
+//       return builder;
+//     },
+//     /**
+//      * Method to append children to the paragraph element.
+//      * @param newChildren Additional children (strings or DOM nodes) to append to the paragraph element.
+//      * @returns The builder object for method chaining.
+//      */
+//     children(...newChildren: Array<string | Node>) {
+//       appendChildren(paragraph, newChildren);
+//       return builder;
+//     },
+//     /**
+//      * Method to build and return the final HTML paragraph element.
+//      * @returns The final HTML paragraph element with all specified attributes and children.
+//      */
+//     build() {
+//       return paragraph;
+//     },
+//   };
+//
+//   return builder;
+// };
 
 export default p;
